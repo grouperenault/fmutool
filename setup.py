8b85c536aedae8e8d079c8f507e9ce6a5c798609
Nicolas LAURENT <nl@haplo.info>
2024-06-17 22:00:46 +0200

    Improve packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from fmutool.version import __author__ as author, __version__ as default_version
 
 try:
-    version = os.environ["CI_COMMIT_REF_NAME"] + "-" + os.environ["CI_COMMIT_SHORT_SHA"]
+    version = os.environ["GITHUB_REF_NAME"]
 except Exception as e:
     print(f"Cannot get repository status: {e}")
     version = default_version
@@ -17,6 +17,10 @@ setup(
                               "remoting/win32/server_sm.exe",
                               "remoting/win64/client_sm.dll",
                               "remoting/win64/server_sm.exe",
+                              "remoting/linux64/client_sm.so",
+                              "remoting/linux64/server_sm",
+                              "remoting/linux32/client_sm.so",
+                              "remoting/linux32/server_sm",
                               "remoting/license.txt",
                               "resources/*.png"],
                   },
